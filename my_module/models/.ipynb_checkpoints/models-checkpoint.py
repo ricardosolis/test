d51b19230d2b52ab9535f2766a22ec2e32644c2f
Ricardo Solis <ricardo.solis@thomasgreg.com>
2020-01-22 13:29:36 +0000

    [IMP] my_module: automatically remove special chars in my_module.my_module name field

--- my_module/models/.ipynb_checkpoints/models-checkpoint.py
+++ my_module/models/.ipynb_checkpoints/models-checkpoint.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from unidecode import unidecode
 from odoo import models, fields, api
 
 
@@ -11,6 +12,19 @@ class my_module(models.Model):
     value = fields.Integer()
     value2 = fields.Float(compute="_value_pc", store=True)
     start_datetime = fields.Datetime('Start time', default=lambda self: fields.Datetime.now())
+    
+    @api.model
+    def create(self, values):
+        if 'name' in values:
+            values['name'] = unidecode(values['name'])
+        return super(my_module, self).create(values)
+
+    @api.multi
+    def write(self, values):
+        if 'name' in values:
+            values['name'] = unidecode(values['name'])
+        return super(my_module, self).write(values)    
+    
     description = fields.Text()
 
     @api.depends('value')
